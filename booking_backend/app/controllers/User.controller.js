d28179fe73dd6759099c50f8589e82058c442aa4
Duyb2014733 <ldduy038@gmail.com>
2024-05-25 16:42:36 +0700

    cài controller

---
+++ booking_backend/app/controllers/User.controller.js
@@ -0,0 +1,71 @@
+const UserService = require("../services/User.service");
+const ApiError = require("../api-error");
+const userService = new UserService();
+
+exports.createUser = async (req, res, next) => {
+  try {
+    const userData = req.body;
+    const newUser = await userService.createUser(userData);
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getUserById = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const user = await userService.getUserById(userId);
+    if (!user) {
+      return next(new ApiError(404, "User not found"));
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await userService.getAllUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateUser = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const userData = req.body;
+    const updatedUser = await userService.updateUser(userId, userData);
+    if (!updatedUser) {
+      return next(new ApiError(404, "User not found"));
+    }
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const deletedUser = await userService.deleteUser(userId);
+    if (!deletedUser) {
+      return next(new ApiError(404, "User not found"));
+    }
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteAllUsers = async (req, res, next) => {
+  try {
+    await userService.deleteAllUsers();
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
--- booking_backend/app/controllers/Vehicle.controller.js
+++ booking_backend/app/controllers/Vehicle.controller.js
@@ -1,80 +1,73 @@
 const VehicleService = require("../services/Vehicle.service");
+const vehicleService = new VehicleService();
 
-class VehicleController {
-  constructor() {
-    this.vehicleService = new VehicleService();
+exports.createVehicle = async (req, res, next) => {
+  try {
+    const vehicleData = req.body;
+    const newVehicle = await vehicleService.createVehicle(vehicleData);
+    res.status(201).json(newVehicle);
+  } catch (error) {
+    next(error);
   }
+};
 
-  async createVehicle(req, res, next) {
-    try {
-      const vehicleData = req.body;
-      const newVehicle = await this.vehicleService.createVehicle(vehicleData);
-      res.status(201).json(newVehicle);
-    } catch (error) {
-      next(error);
+exports.getVehicleById = async (req, res, next) => {
+  try {
+    const vehicleId = req.params.id;
+    const vehicle = await vehicleService.getVehicleById(vehicleId);
+    if (!vehicle) {
+      return res.status(404).json({ message: "Vehicle not found" });
     }
+    res.status(200).json(vehicle);
+  } catch (error) {
+    next(error);
   }
+};
 
-  async getVehicleById(req, res, next) {
-    try {
-      const vehicleId = req.params.id;
-      const vehicle = await this.vehicleService.getVehicleById(vehicleId);
-      if (!vehicle) {
-        return res.status(404).json({ message: "Vehicle not found" });
-      }
-      res.status(200).json(vehicle);
-    } catch (error) {
-      next(error);
+exports.getAllVehicles = async (req, res, next) => {
+  try {
+    const vehicles = await vehicleService.getAllVehicles();
+    res.status(200).json(vehicles);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateVehicle = async (req, res, next) => {
+  try {
+    const vehicleId = req.params.id;
+    const vehicleData = req.body;
+    const updatedVehicle = await vehicleService.updateVehicle(
+      vehicleId,
+      vehicleData
+    );
+    if (!updatedVehicle) {
+      return res.status(404).json({ message: "Vehicle not found" });
     }
+    res.status(200).json(updatedVehicle);
+  } catch (error) {
+    next(error);
   }
+};
 
-  async getAllVehicles(req, res, next) {
-    try {
-      const vehicles = await this.vehicleService.getAllVehicles();
-      res.status(200).json(vehicles);
-    } catch (error) {
-      next(error);
+exports.deleteVehicle = async (req, res, next) => {
+  try {
+    const vehicleId = req.params.id;
+    const deletedVehicle = await vehicleService.deleteVehicle(vehicleId);
+    if (!deletedVehicle) {
+      return res.status(404).json({ message: "Vehicle not found" });
     }
+    res.status(204).send();
+  } catch (error) {
+    next(error);
   }
+};
 
-  async updateVehicle(req, res, next) {
-    try {
-      const vehicleId = req.params.id;
-      const vehicleData = req.body;
-      const updatedVehicle = await this.vehicleService.updateVehicle(
-        vehicleId,
-        vehicleData
-      );
-      if (!updatedVehicle) {
-        return res.status(404).json({ message: "Vehicle not found" });
-      }
-      res.status(200).json(updatedVehicle);
-    } catch (error) {
-      next(error);
-    }
+exports.deleteAllVehicles = async (req, res, next) => {
+  try {
+    await vehicleService.deleteAllVehicles();
+    res.status(204).send();
+  } catch (error) {
+    next(error);
   }
-
-  async deleteVehicle(req, res, next) {
-    try {
-      const vehicleId = req.params.id;
-      const deletedVehicle = await this.vehicleService.deleteVehicle(vehicleId);
-      if (!deletedVehicle) {
-        return res.status(404).json({ message: "Vehicle not found" });
-      }
-      res.status(204).send();
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  async deleteAllVehicles(req, res, next) {
-    try {
-      await this.vehicleService.deleteAllVehicles();
-      res.status(204).send();
-    } catch (error) {
-      next(error);
-    }
-  }
-}
-
-module.exports = VehicleController;
+};
