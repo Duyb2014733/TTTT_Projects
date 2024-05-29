4ce7e12564378adb730531b0245d463facb62a4b
Duyb2014733 <ldduy038@gmail.com>
2024-05-29 16:28:25 +0700

    cài service vehicle

---
+++ booking_frontend/src/services/VehicleService.js
@@ -0,0 +1,33 @@
+import createApiClient from "./api.service";
+
+class VehicleService {
+  constructor(baseUrl = "/api/vehicles") {
+    this.api = createApiClient(baseUrl);
+  }
+
+  async createVehicle(vehicleData) {
+    return (await this.api.post("/", vehicleData)).data;
+  }
+
+  async getVehicleById(vehicleId) {
+    return (await this.api.get(`/${vehicleId}`)).data;
+  }
+
+  async getAllVehicles() {
+    return (await this.api.get("/")).data;
+  }
+
+  async updateVehicle(vehicleId, vehicleData) {
+    return (await this.api.put(`/${vehicleId}`, vehicleData)).data;
+  }
+
+  async deleteVehicle(vehicleId) {
+    return (await this.api.delete(`/${vehicleId}`)).data;
+  }
+
+  async deleteAllVehicles() {
+    return (await this.api.delete("/")).data;
+  }
+}
+
+export default new VehicleService();
