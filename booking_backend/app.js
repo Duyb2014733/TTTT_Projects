401b9ba05279cdcf54e8f99c5abb368221e5a1c9
Duyb2014733 <ldduy038@gmail.com>
2024-06-08 13:07:52 +0700

    update backend

--- booking_backend/app.js
+++ booking_backend/app.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const VehicleRouter = require("./app/routes/Vehicle.route");
-const UserRouter = require("./app/routes/User.route");
-const TicketRouter = require("./app/routes/Ticket.route");
-const RouteRouter = require("./app/routes/Route.route");
-const RideRouter = require("./app/routes/Ride.route");
-const ReviewRouter = require("./app/routes/Review.route");
-const PaymentRouter = require("./app/routes/Payment.route");
-const BusStationRouter = require("./app/routes/BusStation.route");
-const BookingRouter = require("./app/routes/Booking.route");
+const KhachHangRouter = require("./app/routes/KhachHang.route");
+const ChuyenXeRouter = require("./app/routes/ChuyenXe.route");
+const NhaXeRouter = require("./app/routes/NhaXe.route");
+const ThanhToanRouter = require("./app/routes/ThanhToan.route");
+const TuyenDuongRouter = require("./app/routes/TuyenDuong.route");
+const VeRouter = require("./app/routes/Ve.route");
 const ApiError = require("./app/api-error");
 
 const app = express();
@@ -20,15 +17,12 @@ app.get("/", (req, res) => {
   res.json({ message: "Chào mừng bạn đến để kết nối ứng dụng đặt vé." });
 });
 
-app.use("/api/vehicle/", VehicleRouter);
-app.use("/api/user/", UserRouter);
-app.use("/api/ticket/", TicketRouter);
-app.use("/api/route/", RouteRouter);
-app.use("/api/ride/", RideRouter);
-app.use("/api/ride/", ReviewRouter);
-app.use("/api/payment/", PaymentRouter);
-app.use("/api/busstaion/", BusStationRouter);
-app.use("/api/booking/", BookingRouter);
+app.use("/api/khachhang/", KhachHangRouter);
+app.use("/api/chuyenxe/", ChuyenXeRouter);
+app.use("/api/nhaxe/", NhaXeRouter);
+app.use("/api/thanhtoan/", ThanhToanRouter);
+app.use("/api/tuyenduong/", TuyenDuongRouter);
+app.use("/api/ve/", VeRouter);
 
 app.use((req, res, next) => {
   return next(new ApiError(404, "Không tìm thấy tài nguyên"));
