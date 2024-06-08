731650d11ad15250f651c158f27db20c44653530
Duyb2014733 <ldduy038@gmail.com>
2024-06-08 13:07:32 +0700

    update route

--- booking_backend/app/routes/TuyenDuong.route.js
+++ booking_backend/app/routes/TuyenDuong.route.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const tuyenDuongController = require("../controllers/TuyenDuong.controller");
 
 router
-  .TuyenDuong("/")
+  .route("/")
   .post(tuyenDuongController.createTuyenDuong)
   .get(tuyenDuongController.getAllTuyenDuongs)
   .delete(tuyenDuongController.deleteAllTuyenDuongs);
 
 router
-  .TuyenDuong("/:id")
+  .route("/:id")
   .get(tuyenDuongController.getTuyenDuongById)
   .put(tuyenDuongController.updateTuyenDuong)
   .delete(tuyenDuongController.deleteTuyenDuong);
