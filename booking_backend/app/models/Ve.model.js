c63b35cf9ebdb9aed5cdcac2e9142ff87a2afe5e
Duyb2014733 <ldduy038@gmail.com>
2024-07-01 09:46:24 +0700

    update model ve

--- booking_backend/app/models/Ve.model.js
+++ booking_backend/app/models/Ve.model.js
@@ -1,24 +1,13 @@
 const mongoose = require("mongoose");
 
 const VeSchema = new mongoose.Schema({
-  customer_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "KhachHang",
-    required: true,
-  },
   trip_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "ChuyenXe",
     required: true,
   },
   seat_number: { type: String, required: true },
-  purchase_date: { type: Date, required: true },
   price: { type: Number, required: true },
-  payment_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "ThanhToan",
-    required: true,
-  },
 });
 
 const Ve = mongoose.model("Ve", VeSchema);
