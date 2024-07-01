5fdcafcecc548525a59b12ee54fc0fc89de7145f
Duyb2014733 <ldduy038@gmail.com>
2024-07-01 12:50:49 +0700

    update model xe

--- booking_backend/app/models/Xe.model.js
+++ booking_backend/app/models/Xe.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const XeSchema = new mongoose.Schema({
   bus_number: { type: String, required: true, unique: true },
   bus_type: { type: String, required: true },
+  bus_seats: { type: Number },
   nha_xe: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "NhaXe",
